b02713fa578d01881f4d66bdccf1d67f1084bd96
Francis Fueconcillo <francisfueconcillo@gmail.com>
2024-05-03 14:52:03 +0800

    Implement processing of Swift and SEPA transactions to MOngoDB

--- solution/worker/tasks.py
+++ solution/worker/tasks.py
@@ -6,9 +6,10 @@ from requests import get
 from celery import Celery
 from celery.schedules import crontab
 from pymongo import MongoClient
-from datetime import datetime
+from datetime import datetime, timedelta
 
-SYSTEMS_API_BASE_URL=os.environ.get('SYSTEMS_API_BASE_URL')
+from utils import *
+
 CELERY_BROKER_URL=os.environ.get('CELERY_BROKER_URL')
 CELERY_BACKEND_URL=os.environ.get('CELERY_BACKEND_URL')
 MONGODB_BASE_URL=os.environ.get('MONGODB_BASE_URL')
@@ -21,58 +22,172 @@ app = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_BACKEND_URL)
 # Define the MongoDB connection
 client = MongoClient(MONGODB_BASE_URL)
 db = client[MONGODB_DB_NAME]
-companies = db['companies']
-process = db['process']
+companies_collection = db['companies']
+process_collection = db['process']
+exchange_rates_collection = db['exchange_rates']
+transactions_collection = db['transactions']
 
-def track_last_record(recordType, _id, ts):
-    last_record_processed = {
-        'type': recordType,
-        'id': _id,
-        'timestamp': ts
-    }
-        
-    process.insert_one(last_record_processed)
-    return last_record_processed
-
-
-def get_last_record(recordType):
-    
-    try:
-        last_record = process.find({'type': recordType}).sort('_id', -1).limit(1)
-        for record in last_record:
-            return record
-        return None
-    except Exception as e:
-        return None
-
+# Calls GET /companies and store it in "companies" collection.
+# The "ibans" field is converted from string to list
+# Processes records by BATCH_SIZE
 @app.task
 def process_companies():
     
-    last_record = get_last_record('company')
+    last_record = get_last_record_processed('company', process_collection)
+    
+    query_params = '?limit=%s' % (BATCH_SIZE)
+    
     if (last_record is not None):
-        url = '%s/companies?limit=%s&after-id=%d' % (SYSTEMS_API_BASE_URL, BATCH_SIZE, last_record['id'])
-    else:
-        url = '%s/companies?limit=%s' % (SYSTEMS_API_BASE_URL, BATCH_SIZE)
-        
-    response = requests.get(url)
+        query_params = '%s&after-id=%s' % (query_params, str(last_record['id']))
 
-    if response.status_code == 200:
-        companies_data = response.json()
-
+    try:
+        companies_data = get_request('/companies', query_params)
         count = len(companies_data)
         if (count):
             for company in companies_data:
                 company['ibans'] = eval(company['ibans'])
 
-            companies.insert_many(companies_data)
-            last_record = track_last_record('company', companies_data[-1]['id'], datetime.now().isoformat() + 'Z')
-            return('%d companies inserted. Last Company ID: %d' % (count, last_record['id']))
+            companies_collection.insert_many(companies_data)
+            company_id = companies_data[-1]['id']
+            save_last_record_processed(
+                'company', 
+                company_id, 
+                datetime.now().isoformat() + 'Z',
+                process_collection
+            )
+            return('%d new companies inserted successfully. Last Company ID processed: %d' % (count, company_id))
         else:
-            return('No new company. Last Company ID: ' + str(last_record['id']))
-             
-    else:
-        return('Failed to fetch data from the Cash Flow API.')
+            return('No new company to process. Last Company ID processed: ' + str(last_record['id']))
+    except (
+        requests.exceptions.Timeout, 
+        requests.exceptions.HTTPError, 
+        requests.exceptions.RequestException
+    ) as e:
+        return('Error:', e)
 
+    
+
+# Calls GET /exchange-rates to get the latest rates
+@app.task
+def process_exchange_rates():
+    exchange_rates = get_request('/exchange-rates')
+    save_last_exchange_rates(
+        exchange_rates, 
+        datetime.now().isoformat() + 'Z',
+        exchange_rates_collection
+    )
+    return exchange_rates
+
+
+
+
+# collection: transactions
+# fields: 
+# - company_id   --- get from companies collection where payer/receiver iban exists in companies.ibans
+# - transaction_id 
+# - amount_raw   --- transaction.amount
+# - currency_raw  --- transaction.currency
+# - amount_usd   --- convert  amount_raw to usd based on usd_rate of currency_raw, on time of processing. (use exchange_rate collection)
+# - amount_eur --- convert  amount_raw to eur based on eur_rate of currency_raw, on time of processing. (use exchange_rate collection)
+# - usd_rate  --- usd_rate on time of processing
+# - eur_rate  --- eur_rate on time of processing
+# - country_code   -- 2-digit country code for payer/receiver
+# - payment_type  --- either 'swift' or 'sepa'
+# - transaction_type --- debit/credit
+
+# NOTE - each raw transaction will create 2 entries in transactions table
+
+@app.task
+def process_swift_trans():
+    exchange_rates = get_last_exchange_rates(exchange_rates_collection)
+    
+    if exchange_rates is None:
+        raise Exception("No exchange_rates found.")
+        
+    last_record = get_last_record_processed('swift_trans', process_collection)
+    
+    query_params = '?limit=%s' % (BATCH_SIZE)
+    
+    if last_record is not None:
+        query_params = '%s&after-id=%s' % (query_params, last_record['id'])
+        
+    try:
+        transactions_data = get_request('/transactions/swift', query_params)
+        count = len(transactions_data)        
+        
+        if count:
+            for transaction in transactions_data:
+                save_transaction(
+                    transaction, 
+                    exchange_rates, 
+                    transactions_collection, 
+                    companies_collection,
+                    payment_type='swift'
+                )
+            
+            save_last_record_processed(
+                'swift_trans', 
+                transactions_data[-1]['id'],
+                transactions_data[-1]['timestamp'],
+                process_collection
+            )
+            return('Processed %s Swift Transactions.' % len(transactions_data))
+        else:
+            return('No Swift Transactions.')
+    
+    except (
+        requests.exceptions.Timeout, 
+        requests.exceptions.HTTPError, 
+        requests.exceptions.RequestException
+    ) as e:
+        return('Error:', e)
+
+@app.task
+def process_sepa_trans():
+    exchange_rates = get_last_exchange_rates(exchange_rates_collection)
+    
+    if exchange_rates is None:
+        raise Exception("No exchange_rates found.")
+        
+    last_record = get_last_record_processed('sepa_trans', process_collection)
+    
+    query_params = '?limit=%s' % (BATCH_SIZE)
+    
+    if last_record is not None:
+        query_params = '%s&after-id=%s' % (query_params, last_record['id'])
+        
+        
+    try:
+        transactions_data = get_request('/transactions/sepa', query_params)
+        count = len(transactions_data)        
+        
+        if count:
+            for transaction in transactions_data:
+                save_transaction(
+                    transaction, 
+                    exchange_rates, 
+                    transactions_collection, 
+                    companies_collection,
+                    payment_type='sepa'
+                )
+            
+            save_last_record_processed(
+                'sepa_trans', 
+                transactions_data[-1]['id'],
+                transactions_data[-1]['timestamp'],
+                process_collection
+            )
+            return('Processed %s SEPA Transactions.' % len(transactions_data))
+        else:
+            return('No SEPA Transactions.')
+    
+    except (
+        requests.exceptions.Timeout, 
+        requests.exceptions.HTTPError, 
+        requests.exceptions.RequestException
+    ) as e:
+        return('Error:', e)
+    
 
 # Configure Celery
 app.conf.beat_schedule = {
@@ -81,18 +196,20 @@ app.conf.beat_schedule = {
         'schedule': crontab(minute=0, hour=0),  # once a day, every midnight
         # 'schedule': 10.0,
     },
-    # 'return-current-time-20-seconds': {
-    #     'task': 'tasks.return_current_time_20',
-    #     'schedule': 20.0,
-    # },
-    # 'return-current-time-30-seconds': {
-    #     'task': 'tasks.return_current_time_30',
-    #     'schedule': 30.0,
-    # },
+    'process_exchange_rates': {
+        'task': 'tasks.process_exchange_rates',
+         'schedule': crontab(minute=0, hour=0),
+        #  'schedule': 10.0,
+    },
+    'process_swift_trans': {
+        'task': 'tasks.process_swift_trans',
+        'schedule': 60.0,   # every 60 seconds
+    },
+    'process_sepa_trans': {
+        'task': 'tasks.process_sepa_trans',
+        'schedule': 60.0,   # every 60 seconds
+    },
 }
-
-
-
 
 if __name__ == '__main__':
     app.start()
---
+++ solution/worker/utils.py
@@ -0,0 +1,130 @@
+import os
+import requests
+
+SYSTEMS_API_BASE_URL=os.environ.get('SYSTEMS_API_BASE_URL')
+
+def get_request(endpoint, query_params=None):
+    if query_params is None:
+        url = '%s%s' % (SYSTEMS_API_BASE_URL, endpoint)
+    else:
+        url = '%s%s%s' % (SYSTEMS_API_BASE_URL, endpoint, query_params)
+        
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful
+        data = response.json()  # Parse the JSON response
+        return data
+    except requests.exceptions.Timeout as e:
+        # Timeout error
+        print("Request timed out!")
+        raise e
+    except requests.exceptions.HTTPError as e:
+        # HTTP error
+        print("HTTP error:", e)
+        raise e
+    except requests.exceptions.RequestException as e:
+        # Other errors
+        print("Error:", e)
+        raise e
+    
+def save_last_record_processed(recordType, id, ts, process_collection):
+    last_record_processed = {
+        'type': recordType,
+        'id': id,
+        'timestamp': ts
+    }
+        
+    process_collection.insert_one(last_record_processed)
+    
+def get_last_record_processed(recordType, process_collection):
+    
+    try:
+        last_record = process_collection.find({'type': recordType}).sort('_id', -1).limit(1)
+        for record in last_record:
+            return record
+        return None
+    except Exception as e:
+        return None
+    
+
+def get_last_exchange_rates(exchange_rates_collection):
+    try:
+        last_record = exchange_rates_collection.find({}).sort('_id', -1).limit(1)
+        for record in last_record:
+            return record['rates']
+        return None
+    except Exception as e:
+        return None
+
+def save_last_exchange_rates(rates, ts, exchange_rates_collection):
+    last_record_processed = {
+        'timestamp': ts,
+        'rates': rates
+    }
+    exchange_rates_collection.insert_one(last_record_processed)
+
+def create_companies_collection_index(companies_collection):
+    if 'ibans_1' not in companies_collection.index_information():
+        # Create index on the 'ibans' field
+        companies_collection.create_index([('ibans', 1)])
+
+def get_company_by_iban(iban, companies_collection):
+    query = {'ibans': {'$in': [iban]}}
+    companies = companies_collection.find(query)
+    for company in companies:
+        return company
+    
+def save_transaction(transaction, exchange_rates, transactions_collection, companies_collection, payment_type='sepa'):
+    payerKey = 'payer'
+    receiverKey = 'receiver'
+    
+    if payment_type=='swift':
+        payerKey = 'sender'
+        receiverKey = 'beneficiary'
+        
+    # process payer
+    company = get_company_by_iban(transaction[payerKey], companies_collection)
+    
+    rates = [item for item in exchange_rates if item['currency'] == transaction['currency']]
+
+    # NOTE: if no exchange rates available for a given transaction, we make them 1:1 with usd and euro
+    if len(rates):
+        rates = rates[0]
+    else: 
+        rates = { 'usd_rate': 1, 'eur_rate': 1 }
+        
+    if company is not None:
+        record = {
+            'company_id': company['id'], 
+            'transaction_id': transaction['id'],
+            'amount_raw': transaction['amount'],
+            'currency_raw': transaction['currency'],
+            'amount_usd': round(transaction['amount'] * rates['usd_rate'], 4) ,
+            'amount_eur': round(transaction['amount'] * rates['eur_rate'], 4),
+            'usd_rate': rates['usd_rate'],
+            'eur_rate': rates['eur_rate'],
+            'country_code': transaction[payerKey][:2],
+            'payment_type': payment_type,
+            'transaction_type': 'debit'
+        }
+
+        transactions_collection.insert_one(record)
+    
+    # process receiver
+    company = get_company_by_iban(transaction[receiverKey], companies_collection)
+    if company is not None:
+        record = {
+            'company_id': company['id'], 
+            'transaction_id': transaction['id'],
+            'amount_raw': transaction['amount'],
+            'currency_raw': transaction['currency'],
+            'amount_usd': round(transaction['amount'] * rates['usd_rate'], 4),
+            'amount_eur': round(transaction['amount'] * rates['eur_rate'], 4),
+            'usd_rate': rates['usd_rate'],
+            'eur_rate': rates['eur_rate'],
+            'country_code': transaction[receiverKey][:2],
+            'payment_type': payment_type,
+            'transaction_type': 'credit'
+        }
+
+        transactions_collection.insert_one(record)
